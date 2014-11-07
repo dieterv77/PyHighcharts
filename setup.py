b059d58b0af0a165dcdcc82a316aa9605cbbf365
Dieter Vandenbussche <dvandenbussche@axioma.com>
2014-11-07 07:45:55 -0500

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyHighcharts',
-    version='0.0.4',
+    version='0.0.5',
     author='Dieter Vandenbussche',
     author_email='',
     packages=['PyHighcharts', 'PyHighcharts.highcharts', 'PyHighcharts.plt_pandas'],
